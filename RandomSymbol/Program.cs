13dde277405b53a370eef92fb6729734ab2b4c9a
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2024-05-15 20:57:31 +0500

    Добавьте файлы проекта.

---
+++ RandomSymbol/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RandomSymbol
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = "note1.txt";
+			using(StreamReader reader = new StreamReader(path))
+			{
+				Console.WriteLine(reader.ReadToEnd());
+			};
+			char[] Alphabet =
+				{
+				'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+				};
+			bool isWorking = true;
+			
+			var RandomSymbolObj = new RandomSymbolClass();
+			Console.WriteLine("Данная программа выводит случайный символ русского алфавита по нажатии любой кнопки!");
+			Console.ReadKey();
+			//Вызвать случайный символ
+			while (isWorking)
+			{
+				ConsoleKeyInfo userInput = Console.ReadKey();
+				RandomSymbolObj.RandomColor();
+				RandomSymbolObj.RandomSymbol(Alphabet, userInput);
+
+				
+
+
+			}
+			
+			
+		}
+		
+	}
+}
---
+++ RandomSymbol/RandomSymbolClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSymbol
+{
+	public class RandomSymbolClass
+	{
+		public void RandomSymbol(char[] Alphabet, ConsoleKeyInfo userInput)
+		{
+			
+			Random random = new Random();
+			int randomNumber = random.Next(0, Alphabet.Length);
+			//вывод
+			//RandomColor();
+			
+			if(userInput.Key == ConsoleKey.Enter)
+			{
+			Console.WriteLine(Alphabet[randomNumber]);
+			}
+			if(userInput.Key == ConsoleKey.Spacebar)
+			{
+			Console.Write(Alphabet[randomNumber] + " ");
+			}
+			else
+			{
+				return;
+			}
+			
+			using (StreamWriter sw = new StreamWriter(@"C:\Users\kenot\source\repos\RandomSymbol\RandomSymbol\bin\Debug\GameLog.txt",true))
+			{
+				
+				sw.Write(Alphabet[randomNumber] + " ");
+				
+				//foreach (char symbol in Alphabet)
+				//{
+				//	sw.WriteLine(symbol);
+				//}
+
+			}
+			
+
+
+		}
+		public void RandomColor()
+		{
+			string[] Colors = { "Red", "Green", "Blue", "Yellow", "Cyan", "Magenta" };
+			Random randomColor = new Random();
+			int randomIndex = randomColor.Next(0, Colors.Length);
+			ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Colors[randomIndex]);
+			Console.ForegroundColor = color;
+		}
+
+		
+		
+	}
+}
